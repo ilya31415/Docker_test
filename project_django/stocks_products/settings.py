c0f8572e1015c070a7bca975f85f6d09cd306c5b
(0_0) <frostilia@mail.ru>
2022-08-14 22:51:57 +0300

    docker-compose

--- project_django/stocks_products/settings.py
+++ project_django/stocks_products/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'qwe',
+        'HOST': 'localhost',
+        'PORT': '5433',
+        'USER': 'aio',
+        'PASSWORD': '1234'
     }
 }
 
